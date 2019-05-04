41a86abd087a6eecf3764f1f5acc231dfca25f51
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-04 11:54:38 -0400

    add View tag with bg style

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { viewStyle, textStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8'
+  },
   textStyle: {
     fontSize: 20
   }
