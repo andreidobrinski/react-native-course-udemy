4705207ef4fa62c47f385c53e4738a0e16bf18c6
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-05 12:07:08 -0400

    add card styling

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -10,7 +10,8 @@ class AlbumList extends Component {
 
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => this.setState({ albums: response.data }));
+      .then(response => this.setState({ albums: response.data }))
+      .catch(response => console.log(response));
   }
 
   renderAlbums() {
@@ -20,6 +21,7 @@ class AlbumList extends Component {
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         {this.renderAlbums()}
