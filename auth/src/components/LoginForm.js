e33b3ed3875fa594e93c927f07c4369860147e14
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-06 18:11:30 -0400

    use custom Input component in LoginForm

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = { text: '' };
 
   render() {
     return (
-      <View>
-        <Card>
-          <CardSection>
-            <TextInput
-              value={this.state.text}
-              onChangeText={text => this.setState({ text })}
-              style={{ height: 20, width: 100 }}
-            />
-          </CardSection>
-          <CardSection />
-          <CardSection>
-            <Button>
-              Log in
-            </Button>
-          </CardSection>
-        </Card>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
+        <CardSection />
+        <CardSection>
+          <Button>
+            Log in
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
