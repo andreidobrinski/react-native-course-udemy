ab1ab3d9a8efc9f861f2323ee7bfe9d90fbc4200
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-06 22:46:11 -0400

    add Spinner to LoginForm

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { Text } from 'react-native';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
     error: '',
+    loading: false,
   };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    this.setState({ error: '' });
+    this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
       .catch(() => {
@@ -22,6 +23,18 @@ class LoginForm extends Component {
             this.setState({ error: 'Authentication Failed' });
           });
       });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
   }
 
   render() {
@@ -48,9 +61,7 @@ class LoginForm extends Component {
           {this.state.error}
         </Text>
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
