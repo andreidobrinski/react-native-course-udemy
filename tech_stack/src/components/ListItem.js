041d09490e71d4a19e5b0a936dbd755affa1d2df
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-08 16:07:59 -0400

    add LayoutAnimation for spring

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { description } = this.props.library.item;
     const { expanded } = this.props;
 
     if (expanded) {
       return (
-        <Text>{description}</Text>
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            {description}
+          </Text>
+        </CardSection>
       );
     }
   }
