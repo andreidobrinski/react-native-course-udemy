9afcc77eaa485a6f7c401015d52dcd2c210fc0e8
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-09 16:49:35 -0400

    add action creator for email changed

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 
-const LoginForm = () => (
-  <Card>
-    <CardSection>
-      <Input
-        label="Email"
-        placeholder="email@email.com"
-      />
-    </CardSection>
-    <CardSection>
-      <Input
-        label="Password"
-        placeholder="password"
-        secureTextEntry
-      />
-    </CardSection>
-    <CardSection>
-      <Button>
-        Login
-      </Button>
-    </CardSection>
-  </Card>
-);
+
+class LoginForm extends Component {
+  onEmailChange(text) {
+
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@email.com"
+            onChangeText={this.onEmailChange.bind(this)}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            label="Password"
+            placeholder="password"
+            secureTextEntry
+          />
+        </CardSection>
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
 
 export default LoginForm;
