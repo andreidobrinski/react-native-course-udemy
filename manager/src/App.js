085c6feb1cc569086aab1940b5c61aa4ed9cee7f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-09 15:57:56 -0400

    setup firebase

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import { firebaseConfig } from './firebaseConfig';
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
