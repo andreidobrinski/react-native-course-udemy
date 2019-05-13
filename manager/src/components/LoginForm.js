827cdfaa8afaaaf2edf0124d40b00c8ddada95ef
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-13 16:45:15 -0400

    add password redux flow

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -8,6 +8,10 @@ class LoginForm extends Component {
     this.props.emailChanged(text);
   }
 
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
   render() {
     return (
       <Card>
@@ -24,6 +28,8 @@ class LoginForm extends Component {
             label="Password"
             placeholder="password"
             secureTextEntry
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
           />
         </CardSection>
         <CardSection>
@@ -38,8 +44,9 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
