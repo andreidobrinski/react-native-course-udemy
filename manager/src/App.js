17d3641ccd92fde93f8948818ed902125b7b3c88
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-05-14 15:37:35 -0400

    add thunk action for login

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
 import { firebaseConfig } from './firebaseConfig';
@@ -12,8 +13,9 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
